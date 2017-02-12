5855c1831d3a258a70563b2b61c7e7bedfe159e1
Chase Cross <chaseacross@gmail.com>
2017-02-11 19:58:06 -0800

    Trying to get table to display -- so far nothing

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//STARTER CODE//
 var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
 
 var pikePlace = {
   name: 'Pike Place',
-  minCust: 17,
+  min: 17,
   max: 88,
   avg: 5.2,
   avgCookiesPerCusty: [],
@@ -16,4 +15,23 @@ pikePlace.custiesPerHour = function() {
   var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
     this.avgCookiesPerCusty.push(hourlyCusty);
   }
+  console.log('Custies Per Hour fired!');
 };
+//Multiply custies per hour by average cookies per hour//
+pikePlace.cookiesPerCusty = function() {
+  this.custiesPerHour();
+  for (var i = 0; i < hours.length; i++) {
+    var hourCookies = Math.ceil(this.avgCookiesPerCusty[i] * this.avg);
+    this.cookiesPerHour.push(hourCookies);
+    this.totalCookies += hourCookies;
+  }
+  console.log('Cookies per hour fired!');
+}
+//Showing results in table on Index//
+var ul = document.getElementById('pikePlace');
+ul.textContent = pikePlace.name;
+for (var i = 0; i < hours.length; i++) {
+  var li = document.createElement('li');
+  li.textContent = hours[i] + ' ' + this.cookiesPerHour[i];
+  ul.appendChild(li);
+}
