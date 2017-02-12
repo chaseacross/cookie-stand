4160b5205ae336b5b8d0a5a9bc02ce2710347b0c
Chase Cross <chaseacross@gmail.com>
2017-02-11 18:29:52 -0800

    Calculating custies per hour

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
 
 var pikePlace = {
-  min: 17,
+  name: 'Pike Place',
+  minCust: 17,
   max: 88,
   avg: 5.2,
-  // What other properties might you need to keep track of?
-  function getCustomersPerHour(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  };
-var cookiesPerHour = [];
-generateCookies:function(){
-  for (i=0; i<8; i++){
-    )
+  avgCookiesPerCusty: [],
+  cookiesPerHour: [],
+  totalCookies: 0,
+};
+//Figuring out Customers Per Hour -- will multiply by average order to find cookies per hour//
+pikePlace.custiesPerHour = function() {
+  for ( var i = 0; i < hours.length; i++) {
+  var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    this.avgCookiesPerCusty.push(hourlyCusty);
   }
-}
+};
