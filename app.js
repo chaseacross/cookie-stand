c7572b1a72530b722cdd673e7a43aecf22527bb8
Chase Cross <chaseacross@gmail.com>
2017-02-11 23:11:37 -0800

    Created rudimentary style guide per Problem Domain

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
 
+//Hours variable -- same for all stores//
+var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
+//First store: Pike Place -- properties//
 var pikePlace = {
   name: 'Pike Place',
   min: 17,
@@ -9,7 +11,7 @@ var pikePlace = {
   cookiesPerHour: [],
   totalCookies: 0,
 };
-//Figuring out Customers Per Hour -- will multiply by average order to find cookies per hour//
+//Figuring out Customers Per Hour -- will multiply by average cookies per order to find cookies per hour//
 pikePlace.custiesPerHour = function() {
   for (var i = 0; i < hours.length; i++) {
   var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
@@ -17,7 +19,7 @@ pikePlace.custiesPerHour = function() {
   }
   console.log('Custies Per Hour fired!');
 };
-//Multiply custies per hour by average cookies per hour//
+//Multiply custies per hour by average cookies per hour, fill array with one answer per hour//
 pikePlace.cookiesPerCusty = function() {
   this.custiesPerHour();
   for (var i = 0; i < hours.length; i++) {
@@ -27,10 +29,10 @@ pikePlace.cookiesPerCusty = function() {
   }
   console.log('Cookies per hour fired!');
 }
-//Showing results in table on Index//
+// Showing results in table on Index -- just trying to get results to display, no luck so far//
 var div = document.getElementById('pikePlace');
     ul = document.createElement('ul');
-for(var i in pikePlace.cookiesPerHour) {
+for (var i in pikePlace.cookiesPerHour) {
 	var li = document.createElement('li'),
 		  content = document.createTextNode(pikePlace.cookiesPerHour[i]);
   li.appendChild(content);
