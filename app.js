2d7602a0bb84db92123297fd9515f6b828340665
Chase Cross <chaseacross@gmail.com>
2017-02-13 01:11:02 -0800

    Got cookie store to render core list -- just need to add total

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
+//STARTER CODE//
+
 
 //Hours variable -- same for all stores//
 var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
+
 //First store: Pike Place -- properties//
 var pikePlace = {
   name: 'Pike Place',
   min: 17,
   max: 88,
   avg: 5.2,
-  avgCookiesPerCusty: [],
+  avgCookiesPerSale: [],
   cookiesPerHour: [],
-  totalCookies: 0,
+  total: 0,
 };
+
 //Figuring out Customers Per Hour -- will multiply by average cookies per order to find cookies per hour//
 pikePlace.custiesPerHour = function() {
-  for (var i = 0; i < hours.length; i++) {
-  var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    this.avgCookiesPerCusty.push(hourlyCusty);
+  for ( var i = 0; i < hours.length; i++) {
+    var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    this.avgCookiesPerSale.push(hourlyCusty);
   }
-  console.log('Custies Per Hour fired!');
-};
+},
+
 //Multiply custies per hour by average cookies per hour, fill array with one answer per hour//
-pikePlace.cookiesPerCusty = function() {
+pikePlace.cookiesPerCustomer = function() {
   this.custiesPerHour();
   for (var i = 0; i < hours.length; i++) {
-    var hourCookies = Math.ceil(this.avgCookiesPerCusty[i] * this.avg);
-    this.cookiesPerHour.push(hourCookies);
-    this.totalCookies += hourCookies;
+    var singleHourCookies = Math.ceil(this.avgCookiesPerSale[i] * this.avg);
+    this.cookiesPerHour.push(singleHourCookies);
+    this.total += singleHourCookies;
   }
-  console.log('Cookies per hour fired!');
-}
+},
+
 // Showing results in table on Index -- just trying to get results to display, no luck so far//
-var div = document.getElementById('pikePlace');
-    ul = document.createElement('ul');
-for (var i in pikePlace.cookiesPerHour) {
-	var li = document.createElement('li'),
-		  content = document.createTextNode(pikePlace.cookiesPerHour[i]);
-  li.appendChild(content);
-  ul.appendChild(li);
-}
-div.appendChild(ul);
+pikePlace.render = function() {
+  var pikePlaceList = document.getElementById('pikePlaceList');
+  this.cookiesPerCustomer();
+  for(var i = 0; i < this.cookiesPerHour.length; i++) {
+    var pikePlaceSales = document.createElement('li');
+    pikePlaceSales.textContent = hours[i] + this.cookiesPerHour[i] + ' cookies';
+    pikePlaceList.appendChild(pikePlaceSales);
+  }
+};
+
+pikePlace.render();
