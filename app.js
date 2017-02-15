b6166500d62724a800edbe26e12e361f4c63ab6a
Chase Cross <chaseacross@gmail.com>
2017-02-14 19:54:55 -0800

    Code finally functioning properly -- table rendering with MVP results

--- app.js
+++ app.js
@@ -1,81 +1,83 @@
-'use strict'
-var hours = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
+//Establishing top-level variables//
+
+var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 var stores = [];
 var table;
-//Deleted existing code -- will be using constructor to generate stores -- using 2-9-2017 pull as reference//
 
-//Recreated all elements from one store per example from lecture//
-function CookieStore(name, min, max, avgCookiesPerSale){
-  this.name = name;
+//Basic cookie store constructor function with all elements of a cookie store//
+
+function CookieStore(store, min, ma, avgCustomer) {
+  this.store = store;
   this.min = min;
-  this.max = max;
-  this.avg = avg;
-  this.total = 0;
-  this.avgCookiesPerSale = [];
-  this.cookiesPerHour = [];
+  this.ma = ma;
+  this.avgCustomer = avgCustomer;
+  this.perHourTotals = [];
+  this.grandTotal = 0;
   stores.push(this);
 
-this.hourTotal ();
-renderStore(table, this);
+//This code was referred to me by partner -- neither of us is 100% sure how/why it is necessary//
+  this.hourTotal();
+  renderStore(table, this) ;
 }
-
-//Creating first element -- figuring out customers per hour//
-CookieStore.prototype.custiesPerHour = function() {
-  for ( var i = 0; i < hours.length; i++) {
-    var hourlyCusty = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    this.avgCookiesPerSale.push(hourlyCusty);
-  }
+//Customer generator -- random number between min and max//
+CookieStore.prototype.getRandom = function(min, max) {
+  return Math.random() * (max - min) + min;
 };
 
-//Creating per store cookie totals//
-CookieStore.prototype.cookiesPerCustomer = function() {
-  this.custiesPerHour();
+//Uses random customer number and applies a random number of cookies purchased by customer based on the average number of cookies from store data//
+CookieStore.prototype.hourTotal = function() {
   for (var i = 0; i < hours.length; i++) {
-    var singleHourCookies = Math.ceil(this.avgCookiesPerSale[i] * this.avg);
-    this.cookiesPerHour.push(singleHourCookies);
-    this.total += singleHourCookies;
+    var averageCookiesPerCusty = Math.floor(this.avgCustomer * this.getRandom(this.min, this.ma));
+    this.perHourTotals.push(averageCookiesPerCusty);
+    this.grandTotal += averageCookiesPerCusty;
   }
 };
 
-//Generating table and labeling its elements//
+//This function renders the final table -- neither me nor partner sure why it is in parantheses//
 (function renderTable() {
   table = document.createElement('table');
   table.id = 'table';
   var nameCell = document.createElement('tr');
-  var name = document.createElement('th');
-  name.textContent = '';
-  nameCell.appendChild(this.name);
+  var store = document.createElement('th');
+  store.textContent = '';
+  nameCell.appendChild(store);
   for (var i = 0; i < hours.length; i++) {
-    var singleHourCookies = document.createElement('th');
-    singleHourCookies.textContent = hours[i];
-    nameCell.appendChild(singleHourCookies);
+    var cookiesPerHour = document.createElement('th');
+    cookiesPerHour.textContent = hours[i];
+    nameCell.appendChild(cookiesPerHour);
   }
   var showsTotal = document.createElement('th');
+//Appends the word "totals" to proper column of table//
   showsTotal.textContent = 'Totals';
   nameCell.appendChild(showsTotal);
   table.appendChild(nameCell);
+  //Places table in html//
   document.getElementById('store_data').appendChild(table);
 })();
 
-function renderCookieStore(table, store) {
+//Constructor for store table data -- note appending within store constructor function//
+function renderStore(table, store) {
     var placeStoreHere = document.createElement('tr');
     var showStoreName = document.createElement('th');
-    showStoreName.textContent = this.name;
+    showStoreName.textContent = store.store;
     placeStoreHere.id = store.id;
     placeStoreHere.appendChild(showStoreName);
+//Loops function for creation of hourly sales totals//
     for (var i = 0; i < hours.length; i++) {
-      var showTotalThisHour = document.createElement('td');
-      showTotalThisHour.textContent = store.perHourTotals[j];
-      placeStoreHere.appendChild(showTotalThisHour);
+      var totalPerHourCell = document.createElement('td');
+      totalPerHourCell.textContent = store.perHourTotals[i];
+      placeStoreHere.appendChild(totalPerHourCell);
     }
+
     var showGrandTotal = document.createElement('td');
-    showGrandTotal.textContent = store.total;
+    showGrandTotal.textContent = store.grandTotal;
     placeStoreHere.appendChild(showGrandTotal);
     table.appendChild(placeStoreHere);
 };
 
+//Calls new stores with data and enters them into the constructor//
 var pikePlace = new CookieStore('Pike Place', 17, 88, 5.2);
-var seaTac = new CookieStore('SeaTac Airport', 6, 24, 1.2);
-var southCenter = CookieStore('Southcenter', 11, 38, 1.9);
-var southCenter = new CookieStore('Bellevue Square', 20, 48, 3.3);
+var seaTac = new CookieStore('SeaTac Airport', 6, 44, 1.2);
+var southCenter = new CookieStore('Southcenter Mall', 11, 38, 1.9);
+var bellevue = new CookieStore('Bellevue Square', 20, 48, 3.3);
 var alki = new CookieStore('Alki', 3, 24, 2.6);
